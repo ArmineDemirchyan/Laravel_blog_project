8e4e36b54cd51b997653ecf216b6a48b9b60184c
ArmineDemirchyan <100238894+ArmineDemirchyan@users.noreply.github.com>
2023-08-18 15:07:21 +0400

    Comment section is ready to add to main branch

--- database/migrations/2023_08_17_112907_create_comments_table.php
+++ database/migrations/2023_08_17_112907_create_comments_table.php
@@ -11,16 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('category_id');
-            $table->string('slug')->unique();
-            $table->string('title');
-            $table->text('excerpt');
+            $table->foreign('post_id')->constrained()->cascadeOnDelete();
+            $table->foreign('user_id')->constrained()->cascadeOnDelete();
+            $table->unsignedInteger('post_id');
+            $table->unsignedInteger('user_id');
             $table->text('body');
             $table->timestamps();
-            $table->timestamp('published_at')->nullable();
         });
     }
 
@@ -29,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 };
--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,16 +1,19 @@
-<article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl" />
-    </div>
+@props(['comment'])
+<x-panel class="bg-gray-50">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/60?u={{$comment->user_id}}" alt="" width="60" height="60" class="rounded-xl" />
+        </div>
 
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
-            <p class="text-xs"><time>8 months ago</time> </p>
-        </header>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. A, vitae. Minima accusantium dolorum blanditiis quo unde sapiente doloribus, voluptatibus quia quis ipsum pariatur, ut dignissimos? Autem odio magni voluptas modi?
-        </p>
-    </div>
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{$comment->author->username}}</h3>
+                <p class="text-xs">Posted <time>{{$comment->created_at->format("F j, Y, g:i a")}}</time> </p>
+            </header>
+            <p>
+                {{$comment->body}}
+            </p>
+        </div>
 
-</article>
+    </article>
+</x-panel>
